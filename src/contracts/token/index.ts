b2bff2bbcb0bc7afde35f6cbaf4ef3628b32a14a
Alex Ant <dacom.dark.sun@gmail.com>
2024-05-08 18:05:02 +0500

    lookink for fix

--- src/contracts/token/index.ts
+++ src/contracts/token/index.ts
@@ -3,9 +3,6 @@ import * as contractNames from '../../common/names'
 export * as Actions from './actions'
 export * as Tables from './tables'
 
-/**
- * @private
- */
 export * as Interfaces from '../../interfaces/token'
 
 export const contractName = contractNames._token
